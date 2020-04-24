3434ed16401b2aaa9ceff38f80eba3efd3f8241b
manoj_niverthi <manojniverthi@gmail.com>
2020-04-24 10:32:20 -0400

    added all state functionality, separate figs

--- viz/state-viz.py
+++ viz/state-viz.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 import pandas as pd
 import json
 import math
+import plotly
 import plotly.express as px
 from plotly.subplots import make_subplots
 
@@ -75,9 +76,80 @@ dataset['statecode'] = dataset['state'].map(us_state_abbrev)
 fig1 = px.choropleth(dataset, template = 'plotly_dark', locationmode = "USA-states", locations = 'statecode', color = 'logcases', 
     hover_name = 'text', animation_frame='date', scope = 'usa', labels={'logcases': 'Number of Cases'}, color_continuous_scale=px.colors.sequential.Plasma)
 fig2 = px.scatter_geo(dataset, scope='usa', template = 'plotly_dark', 
-    locationmode = "USA-states", locations = 'statecode', animation_frame = 'date', size = dataset.deaths.astype(float), hover_name='text')
+    locationmode = "USA-states", locations = 'statecode', animation_frame = 'date', size = dataset.deaths.astype(float)*5, hover_name='text')
+fig3 = go.Figure()
+
+
+for state in dataset.state.unique():
+    dfadj = dataset[dataset['state'] == state]
+    fig3.add_trace(go.Scatter(x=list(dfadj.date), y=list(dfadj.cases), name='Cases in ' + state, mode='markers'))
+    fig3.add_trace(go.Scatter(x=list(dfadj.date), y=list(dfadj.deaths), name='Deaths in ' + state, mode='markers'))
 fig1.show()
 fig2.show()
+# Add dropdown
+# fig3.update_layout(
+#     updatemenus=[
+#         dict(
+#             buttons=list([
+#                 dict(
+#                     args=["type", "cases"],
+#                     label="Cases",
+#                     method="restyle"
+#                 ),
+#                 dict(
+#                     args=["type", "deaths"],
+#                     label="Deaths",
+#                     method="restyle"
+#                 )
+#             ]),
+#             direction="down",
+#             pad={"r": 10, "t": 10},
+#             showactive=True,
+#             x=0.1,
+#             xanchor="left",
+#             y=1.1,
+#             yanchor="top"
+#         ),
+#     ]
+# )
+
+# # Add annotation
+# fig3.update_layout(
+#     annotations=[
+#         dict(text="Trace type:", showarrow=False,
+#         x=0, y=1.085, yref="paper", align="left")
+#     ]
+# )
+# data = [dict(type='choropleth',
+#              locations = dataset['statecode'].astype(str),
+#              z=dataset['total exports'].astype(float),
+#              locationmode='USA-states', 
+#              visible=True)]
+fig3.show()
+# # create the empty dropdown menu
+# updatemenus = list([dict(buttons=list()), 
+#                     dict(direction='down',
+#                          showactive=True)])
+
+# total_codes = len(dataset.code.unique()) + 1
+
+# for s, state in enumerate(dataset.code.unique()):
+#     # add a trace for each state
+#     data.append(dict(type='scatter',
+#                      x=[i for i in range(1980, 2016)],
+#                      y=[i + random.random() * 100 for i in range(1980, 2016)],
+#                      visible=False))
+#     # add each state to the dropdown    
+#     visible_traces = [False] * total_codes
+#     visible_traces[s + 1] = True
+#     updatemenus[0]['buttons'].append(dict(args=[{'visible': visible_traces}],
+#                                           label=state,
+#                                           method='update'))
+
+# # add a dropdown entry to reset the map    
+# updatemenus[0]['buttons'].append(dict(args=[{'visible': [True] + [False] *  (total_codes - 1)}],
+#                                       label='Map',
+#                                       method='update'))
 
 # days = list((set(dataset['date'])))
 # list.sort(days)
